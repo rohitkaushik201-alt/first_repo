164cb6fec2d48c2a768470e85cc7a2f64999d94b
Rohit_Kaushik <rohitkaushik201@gmail.com>
2026-05-01 13:03:57 +0000

    variables initialization

---
+++ 01_basics/01_varables.js
@@ -0,0 +1,15 @@
+const accountId = 233453;
+let accountEmail = "rk@gmail.com";
+var accountPassword = "12345";
+cityName = "fridabad";
+let personName;
+
+
+// accountId = 9876543; // not possible to change constant variable
+console.log(accountId);
+
+accountEmail = "nk@gmail.com";
+accountPassword = "98765";
+cityName = "dehradun";
+
+console.table([accountId,accountEmail,accountPassword,cityName,personName])
