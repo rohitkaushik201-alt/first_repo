d6c04b28fe24287b83cb67ec3d47c4672c71b81c
Rohit_Kaushik <rohitkaushik201@gmail.com>
2026-05-03 09:56:59 +0000

    some 01_datatypes

--- 01_basics/01_varables.js
+++ 01_basics/01_varables.js
@@ -8,6 +8,10 @@ let personName;
 // accountId = 9876543; // not possible to change constant variable
 console.log(accountId);
 
+/*Prefer not to use var 
+because of issue of block scope and functional scope
+*/ 
+
 accountEmail = "nk@gmail.com";
 accountPassword = "98765";
 cityName = "dehradun";
